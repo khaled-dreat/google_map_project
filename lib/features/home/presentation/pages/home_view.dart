64dd4c280ebe77fdf3d96e2750127f8f4fcb0679
khaled-dreat <dreatkhaled@gmail.com>
2024-04-26 00:01:58 +0300

    Add initial markers to Google Map in HomeView
    Initialize markers on the Google Map in HomeView using custom markers for
    places defined in PlaceModel. Also, load map style and set initial camera
    position.

--- lib/features/home/presentation/pages/home_view.dart
+++ lib/features/home/presentation/pages/home_view.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_map_project/features/home/data/models/m_palce.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -19,50 +11,59 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
-  int _counter = 0;
+late GoogleMapController googleMapController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    initMarkers();
+    super.initState();
+  }
+
+  void initMapStyle() async {
+    var nighMapStyle = await DefaultAssetBundle.of(context)
+        .loadString("assets/json/google_map_style.json");
+    googleMapController.setMapStyle(nighMapStyle);
+    initMarkers();
+  }
+
+  Set<Marker> markers = {};
+  void initMarkers() async {
+    var customMarcker = await BitmapDescriptor.fromAssetImage(
+        const ImageConfiguration(), "assets/map-pointer-house-svgrepo-com.png");
+    var myMarkers = places
+        .map(
+          (placeModel) => Marker(
+            icon: customMarcker,
+            infoWindow: InfoWindow(title: placeModel.name),
+            position: placeModel.latLng,
+            markerId: MarkerId(
+              placeModel.id.toString(),
+            ),
+          ),
+        )
+        .toSet();
+    markers.addAll(myMarkers);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: GoogleMap(
-            cameraTargetBounds: CameraTargetBounds(LatLngBounds(
-                southwest: LatLng(31, 29), northeast: LatLng(31, 30))),
+            markers: markers,
+            onMapCreated: (controller) {
+              googleMapController = controller;
+              initMapStyle();
+            },
             initialCameraPosition: CameraPosition(
                 zoom: 12,
-                target: LatLng(
-                  30,
-                  30,
-                ))),
+                target: LatLng(31.186070052677902, 29.93063447509182))),
       ),
     );
   }
