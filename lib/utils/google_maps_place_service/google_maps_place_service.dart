d57d57738986c59ab19d7e6a2b65a10748eb9b16
khaled-dreat <dreatkhaled@gmail.com>
2024-05-01 18:13:22 +0300

    Refactor GoogleMapsPlaceService methods
    Update getPredictions and getPlaceDetails methods in GoogleMapsPlaceService
    to include necessary parameters and improve code readability.

--- lib/utils/google_maps_place_service/google_maps_place_service.dart
+++ lib/utils/google_maps_place_service/google_maps_place_service.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 
 import 'package:flutter_with_google_maps/models/place_autocomplete_model/place_autocomplete_model.dart';
+import 'package:flutter_with_google_maps/models/place_details_model/place_details_model.dart';
 import 'package:http/http.dart' as http;
 
 class GoogleMapsPlaceService {
   final String baseUrl = 'https://maps.googleapis.com/maps/api/place';
   final String apiKey = 'AIzaSyCRKcVVWtCfa1TCSfMtfNJ599N_jrNUux4';
 
-  Future<List<PlaceModel>> getPredictions({required String input}) async {
-    var response = await http
-        .get(Uri.parse('$baseUrl/autocomplete/json?key=$apiKey&input=$input'));
+  Future<List<PlaceModel>> getPredictions(
+      {required String input, required String sesstionToken}) async {
+    var response = await http.get(Uri.parse(
+        '$baseUrl/autocomplete/json?key=$apiKey&input=$input&sessiontoken=$sesstionToken'));
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body)['predictions'];
       List<PlaceModel> places = [];
@@ -22,16 +24,13 @@ class GoogleMapsPlaceService {
     }
   }
 
-  Future<List<PlaceModel>> getPlacesDetails({required String input}) async {
+  Future<PlaceDetailsModel> getPlaceDetails({required String placeId}) async {
     var response = await http
-        .get(Uri.parse('$baseUrl/autocomplete/json?key=$apiKey&input=$input'));
+        .get(Uri.parse('$baseUrl/details/json?key=$apiKey&place_id=$placeId'));
+
     if (response.statusCode == 200) {
-      var data = jsonDecode(response.body)['predictions'];
-      List<PlaceModel> places = [];
-      for (var element in data) {
-        places.add(PlaceModel.fromJson(element));
-      }
-      return places;
+      var data = jsonDecode(response.body)['result'];
+      return PlaceDetailsModel.fromJson(data);
     } else {
       throw Exception();
     }
